8e0206e3939368de09539390f2005efa1f617c40
duxiaofeng-github <csdxf297951292@gmail.com>
2025-01-07 16:27:58 +0800

    update UI

--- webui/src/components/vaultinfo/ClaimForm.tsx
+++ webui/src/components/vaultinfo/ClaimForm.tsx
@@ -1,22 +1,26 @@
-import {
-	useAccount,
-  useReadContract,
-  useWriteContract,
-} from "wagmi";
+import { useAccount, useReadContract, useWriteContract } from "wagmi";
 import { ConfigForChainId } from "../../utils/chaincfg";
 
 import FundingVaultAbi from "../../abi/FundingVault.json";
 import { useEffect, useState } from "react";
-import { toBigintUnit, toDecimalUnit, toReadableAmount, toReadableDuration } from "../../utils/ConvertHelpers";
+import {
+  toBigintUnit,
+  toDecimalUnit,
+  toReadableAmount,
+  toReadableDuration,
+} from "../../utils/ConvertHelpers";
 import { isAddress } from "ethers";
-
-import "./ClaimForm.css"
+import { GlassButton } from "../button/GlassButton";
+import forkIcon from "../../assets/imgs/fork.png";
+import currencyDollarIcon from "../../assets/imgs/currency-dollar.png";
+import checkCircleIcon from "../../assets/imgs/check-circle.png";
+import xCircleIcon from "../../assets/imgs/x-circle.png";
 
 interface IGrantDetails {
-  claimInterval: bigint
-  claimLimit: bigint
-  claimTime: bigint
-  dustBalance: bigint
+  claimInterval: bigint;
+  claimLimit: bigint;
+  claimTime: bigint;
+  dustBalance: bigint;
 }
 
 const ClaimForm = (props: { grantId: number }): React.ReactElement => {
@@ -25,24 +29,24 @@ const ClaimForm = (props: { grantId: number }): React.ReactElement => {
   let [claimAmount, setClaimAmount] = useState("10");
   let [claimTarget, setClaimTarget] = useState("");
   let [claimAll, setClaimAll] = useState<boolean>(false);
-  let [claimTargetCustom, setClaimTargetCustom] = useState<boolean>(false);
+  let [claimTargetCustom, setClaimTargetCustom] = useState<boolean>(true);
 
   const grantDetails = useReadContract({
-		address: chainConfig.VaultContractAddr,
+    address: chainConfig.VaultContractAddr,
     account: address,
-		abi: FundingVaultAbi,
-		chainId: chainConfig.Chain.id,
-		functionName: "getGrant",
-		args: [ props.grantId ],
-	});
+    abi: FundingVaultAbi,
+    chainId: chainConfig.Chain.id,
+    functionName: "getGrant",
+    args: [props.grantId],
+  });
   const claimableBalance = useReadContract({
-		address: chainConfig.VaultContractAddr,
+    address: chainConfig.VaultContractAddr,
     account: address,
-		abi: FundingVaultAbi,
-		chainId: chainConfig.Chain.id,
-		functionName: "getClaimableBalance",
-		args: [ props.grantId ],
-	});
+    abi: FundingVaultAbi,
+    chainId: chainConfig.Chain.id,
+    functionName: "getClaimableBalance",
+    args: [props.grantId],
+  });
   const claimRequest = useWriteContract();
 
   //console.log(grantDetails.data);
@@ -55,142 +59,219 @@ const ClaimForm = (props: { grantId: number }): React.ReactElement => {
       clearInterval(interval);
     };
   }, [claimableBalance]);
-  
-  let maxAmount = toDecimalUnit(claimableBalance.data as bigint, chain?.nativeCurrency.decimals);
-  if(isNaN(maxAmount)) {
+
+  let maxAmount = toDecimalUnit(
+    claimableBalance.data as bigint,
+    chain?.nativeCurrency.decimals
+  );
+  if (isNaN(maxAmount)) {
     maxAmount = 0;
   }
   maxAmount = Math.round(maxAmount * 1000) / 1000;
 
-  if(parseInt(claimAmount) > maxAmount) {
+  if (parseInt(claimAmount) > maxAmount) {
     setClaimAmount(maxAmount.toString());
-  } else if(parseInt(claimAmount) < 0) {
+  } else if (parseInt(claimAmount) < 0) {
     setClaimAmount("0");
   }
 
   return (
     <div>
-      <table className="details-table">
-        <tbody>
-          <tr>
-            <td className="prop">Your claimable balance:</td>
-            <td className="value">{toReadableAmount(claimableBalance.data as bigint, chain?.nativeCurrency.decimals, chainConfig.TokenName, 3)}</td>
-          </tr>
-          {grantDetails.data ?
-          <tr>
-            <td className="prop">Your allowance:</td>
-            <td className="value">{toReadableAmount((grantDetails.data as IGrantDetails)?.claimLimit, 0, chainConfig.TokenName, 0)} per {toReadableDuration((grantDetails.data as IGrantDetails)?.claimInterval)}</td>
-          </tr>
-          : null}
-        </tbody>
-      </table>
-      <div className="claim-form container">
-        <b>Claim Funds</b>
-
+      <div className="ata-claim-info">
+        <div className="ata-claim-info-section">
+          <span className="ata-claim-info-label">Claimable balance:</span>
+          <span className="ata-claim-info-content">
+            {toReadableAmount(
+              claimableBalance.data as bigint,
+              chain?.nativeCurrency.decimals,
+              chainConfig.TokenName,
+              3
+            )}
+          </span>
+        </div>
+        <div className="ata-claim-info-section">
+          <span className="ata-claim-info-label">Earning rate:</span>
+          <span className="ata-claim-info-content">
+            {toReadableAmount(
+              (grantDetails.data as IGrantDetails)?.claimLimit,
+              0,
+              chainConfig.TokenName,
+              0
+            )}{" "}
+            per{" "}
+            {toReadableDuration(
+              (grantDetails.data as IGrantDetails)?.claimInterval
+            )}
+          </span>
+        </div>
+      </div>
+      <div className="ata-claim-form">
         <div className="row mt-2">
-          <div className="col-6">
+          <div className="col-4 ata-form-label">
             Amount ({chainConfig.TokenName})
           </div>
-          <div className="col-6">
-            <div className="form-check">
-              <input className="form-check-input" type="checkbox" value="" id="claimAll" onChange={(evt) => setClaimAll(evt.target.checked)} checked={claimAll} />
-              <label className="form-check-label" htmlFor="claimAll">
-                Claim all claimable balance
-              </label>
-            </div>
-          </div>
+          <div className="col-8 ata-form-label">Target Wallet</div>
         </div>
         <div className="row">
-          <div className="col-5">
-            <input type="number" className="form-control" placeholder={claimAll ? maxAmount.toString() : "0"} onChange={(evt) => setClaimAmount(evt.target.value)} value={claimAll ? maxAmount.toString() : claimAmount} disabled={claimAll} />
+          <div className="col-4">
+            <div className="ata-input-container">
+              <input
+                type="number"
+                className="form-control ata-input"
+                placeholder={claimAll ? maxAmount.toString() : "0"}
+                onChange={(evt) => setClaimAmount(evt.target.value)}
+                value={claimAll ? maxAmount.toString() : claimAmount}
+                disabled={claimAll}
+              />
+              <GlassButton
+                className="ata-max-button"
+                onClick={() => {
+                  setClaimAmount(maxAmount.toString());
+                }}
+              >
+                MAX.
+              </GlassButton>
+            </div>
           </div>
-          <div className="col-1"></div>
-          <div className="col-6">
-            <input type="range" className="form-range" max={maxAmount} onChange={(evt) => setClaimAmount(evt.target.value)} value={claimAll ? maxAmount.toString() : claimAmount} disabled={claimAll} />
+          <div className="col-8">
+            <div className="ata-input-container">
+              <input
+                type="text"
+                className="form-control ata-input"
+                placeholder={claimTargetCustom ? "0x..." : address}
+                onChange={(evt) => setClaimTarget(evt.target.value)}
+                value={claimTarget}
+                disabled={!claimTargetCustom}
+              />
+              {claimTarget ? (
+                <img
+                  className="ata-fork-icon"
+                  src={forkIcon}
+                  onClick={() => {
+                    setClaimTarget("");
+                  }}
+                />
+              ) : null}
+            </div>
           </div>
         </div>
 
-        <div className="row mt-2">
-          <div className="col-6">
-            Target Wallet
-          </div>
-          <div className="col-6">
-            <div className="form-check">
-              <input className="form-check-input" type="checkbox" value="" id="claimTargetCustom" onChange={(evt) => setClaimTargetCustom(evt.target.checked)} checked={claimTargetCustom} />
-              <label className="form-check-label" htmlFor="claimTargetCustom">
-                Send to another wallet
-              </label>
+        {claimRequest.isPending && (claimRequest.data as any) ? (
+          <div className="ata-notification pending ata-claim-notification">
+            <div className="ata-notification-content">
+              <div className="ata-notification-title">
+                Claim transaction pending...
+              </div>
+              <div className="ata-notification-msg">
+                TX:{" "}
+                <a
+                  href={
+                    chainConfig.BlockExplorerUrl + "tx/" + claimRequest.data
+                  }
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  {claimRequest.data}
+                </a>
+              </div>
             </div>
           </div>
-        </div>
-        <div className="row">
-          <div className="col-12">
-            <input type="text" className="form-control" placeholder={claimTargetCustom ? "0x..." : address} onChange={(evt) => setClaimTarget(evt.target.value)} value={claimTarget} disabled={!claimTargetCustom} />
+        ) : null}
+
+        {claimRequest.isError ? (
+          <div className="ata-notification failed ata-claim-notification">
+            <img className="ata-notification-icon" src={xCircleIcon} />
+            <div className="ata-notification-content">
+              <div className="ata-notification-title">Claim failed.</div>
+              <div className="ata-notification-msg">
+                {(claimRequest.data as any) ? (
+                  <>
+                    TX:{" "}
+                    <a
+                      href={
+                        chainConfig.BlockExplorerUrl + "tx/" + claimRequest.data
+                      }
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      {claimRequest.data}
+                    </a>
+                    .
+                  </>
+                ) : null}
+                {claimRequest.error?.message}
+              </div>
+            </div>
           </div>
-        </div>
+        ) : null}
+
+        {claimRequest.isSuccess ? (
+          <div className="ata-notification success ata-claim-notification">
+            <img className="ata-notification-icon" src={checkCircleIcon} />
+            <div className="ata-notification-content">
+              <div className="ata-notification-title">Claim TX.</div>
+              <div className="ata-notification-msg">
+                {(claimRequest.data as any) ? (
+                  <a
+                    href={
+                      chainConfig.BlockExplorerUrl + "tx/" + claimRequest.data
+                    }
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    {claimRequest.data}
+                  </a>
+                ) : null}
+              </div>
+            </div>
+          </div>
+        ) : null}
 
         <div className="row mt-3">
           <div className="col-12">
-            <button className="btn btn-primary claim-button" onClick={(evt) => requestFunds(evt.target as HTMLButtonElement)} disabled={claimRequest.isPending}>Request Funds</button>
+            <GlassButton
+              className="ata-claim-button"
+              onClick={(evt) => requestFunds(evt.target as HTMLButtonElement)}
+              disabled={claimRequest.isPending}
+            >
+              <img
+                className="ata-currency-dollar-icon"
+                src={currencyDollarIcon}
+              />
+              Request Funds
+            </GlassButton>
           </div>
         </div>
-
-        {claimRequest.isPending && claimRequest.data as any ?
-        <div className="row mt-3">
-          <div className="col-12">
-            <div className="alert alert-info">
-              Claim transaction pending... TX: <a href={chainConfig.BlockExplorerUrl + "tx/" + claimRequest.data} target="_blank" rel="noreferrer">{claimRequest.data}</a>
-            </div>
-          </div>
-        </div>
-        : null}
-        {claimRequest.isError ?
-        <div className="row mt-3">
-          <div className="col-12">
-            <div className="alert alert-danger">
-              Claim failed. {claimRequest.data as any ? <span>TX: <a href={chainConfig.BlockExplorerUrl + "tx/" + claimRequest.data} target="_blank" rel="noreferrer">{claimRequest.data}</a></span> : null}<br />
-              {claimRequest.error.message}
-            </div>
-          </div>
-        </div>
-        : null}
-        {claimRequest.isSuccess ?
-        <div className="row mt-3">
-          <div className="col-12">
-            <div className="alert alert-success">
-              Claim TX: <a className="txhash" href={chainConfig.BlockExplorerUrl + "tx/" + claimRequest.data} target="_blank" rel="noreferrer">{claimRequest.data}</a>
-            </div>
-          </div>
-        </div>
-        : null}
       </div>
-
     </div>
-  )
+  );
 
   function requestFunds(button: HTMLButtonElement) {
     button.disabled = true;
 
     let targetAddress = claimTarget;
-    if(claimTargetCustom && !isAddress(targetAddress)) {
+    if (claimTargetCustom && !isAddress(targetAddress)) {
       alert("Provided target address '" + targetAddress + "' is invalid.");
       button.disabled = false;
       return;
     }
 
     let amount = parseInt(claimAmount);
-    if(claimAll) {
+    if (claimAll) {
       amount = 0;
-    } else if(amount == 0 || amount > maxAmount) {
+    } else if (amount == 0 || amount > maxAmount) {
       alert("Desired amount '" + claimAmount + "' is invalid.");
       button.disabled = false;
       return;
     }
     let amountWei = toBigintUnit(amount, chain?.nativeCurrency.decimals);
 
-    let callfn = "claim"
-    let callArgs: any[] = [ props.grantId, amountWei ];
-    if (claimTargetCustom && targetAddress.toLowerCase() != address?.toLowerCase()) {
+    let callfn = "claim";
+    let callArgs: any[] = [props.grantId, amountWei];
+    if (
+      claimTargetCustom &&
+      targetAddress.toLowerCase() != address?.toLowerCase()
+    ) {
       callfn = "claimTo";
       callArgs.push(targetAddress);
     }
@@ -202,8 +283,8 @@ const ClaimForm = (props: { grantId: number }): React.ReactElement => {
       chainId: chainConfig.Chain.id,
       functionName: callfn,
       args: callArgs,
-    })
+    });
   }
-}
+};
 
 export default ClaimForm;
--- webui/src/components/vaultinfo/VaultInfo.tsx
+++ webui/src/components/vaultinfo/VaultInfo.tsx
@@ -1,46 +1,55 @@
-import {
-	useAccount,
-} from "wagmi";
-import {
-  useConnectModal,
-} from '@rainbow-me/rainbowkit';
+import { useAccount } from "wagmi";
+import { useConnectModal } from "@rainbow-me/rainbowkit";
 import EligibilityCheck from "./EligibilityCheck";
-
+import walletIcon from "../../assets/imgs/wallet.png";
+import { GlassButton } from "../button/GlassButton";
 
 const VaultInfo = (): React.ReactElement => {
   const { isConnected, chain } = useAccount();
   const { openConnectModal } = useConnectModal();
 
   return (
-    <div className="page-block">
-      <h1>Testnet Funding Vault</h1>
-      <p>
-        The FundingVault contract provides a way to distribute continuous limited amounts of funds to authorized entities.<br />
-        The distribution is time gated and a specific limit per grant is enforced.<br />
-        Check out the <a href="https://github.com/ethpandaops/fundingvault/blob/master/README.md">FundingVault repository</a> for more details.
-      </p>
-      {isConnected && chain ?
-      <EligibilityCheck /> : null}
-      {!isConnected ? renderDisconnected() : null}
-      {isConnected && !chain ?  renderInvalidNetwork() : null}
+    <div className="page-block ata-card">
+      <div className="ata-card-content">
+        <h1 className="ata-border-bottom">Automata Funding Vault</h1>
+        <p>
+          The Automata FundingVault contract provides a way to distribute
+          continuous limited amounts of funds to authorized entities.
+          <br />
+          <br />
+          The distribution is time gated and a specific limit per grant is
+          enforced. Check out the{" "}
+          <a href="https://github.com/ethpandaops/fundingvault/blob/master/README.md">
+            FundingVault repository
+          </a>{" "}
+          for more details.
+        </p>
+        {isConnected && chain ? <EligibilityCheck /> : null}
+        {!isConnected ? renderDisconnected() : null}
+        {isConnected && !chain ? renderInvalidNetwork() : null}
+      </div>
     </div>
-  )
+  );
 
   function renderDisconnected() {
     return (
-      <div className="">
-        Please <a href="#" onClick={openConnectModal}>connect to your wallet</a> to continue.
-      </div>
-    )
+      <GlassButton
+        className="connect-btn"
+        onClick={() => {
+          openConnectModal();
+        }}
+      >
+        <img className="wallet-icon" src={walletIcon} />
+        Connect Wallet
+      </GlassButton>
+    );
   }
 
   function renderInvalidNetwork() {
     return (
-      <div className="">
-        Please switch to holesky or sepolia to continue.
-      </div>
-    )
+      <div className="">Please switch to holesky or sepolia to continue.</div>
+    );
   }
-}
+};
 
 export default VaultInfo;
